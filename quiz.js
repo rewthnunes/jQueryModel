78ba20a6d6bcd478496364ce4ce0fd235463a6b4
Tinho <rewth@msn.com>
2020-08-12 19:03:00 -0300

    unico envio

---
+++ quiz.js
@@ -0,0 +1,86 @@
+﻿
+myData = 
+{
+	"quiz": {
+		"question": [
+			{
+				"text": "Sobre ID e CLASS não é correto afirmar:",
+				"answer": [
+					{
+						"_value": 0,
+						"__text": "Servem para identificar e agrupar elementos"
+					},
+					{
+						"_value": 0,
+						"__text": "Servem para aplicar estilos CSS"
+					},
+					{
+						"_value": 0,
+						"__text": "Id são únicos e Class podem se repetir"
+					},
+					{
+						"_value": 0,
+						"__text": "Em Javascript os ID são utilizados ..."
+					},
+					{
+						"_value": 1,
+						"__text": "Usamos IDs para a criação de links do tipo âncora"
+					}
+				],
+				"_id": "1"
+			},
+			{
+				"text": "A pseudo-classe utilizada para indicar o estado de um link quando o ponteiro",
+				"answer": [
+					{
+						"_value": 1,
+						"__text": "hover"
+					},
+					{
+						"_value": 0,
+						"__text": "active"
+					},
+					{
+						"_value": 0,
+						"__text": "link"
+					},
+					{
+						"_value": 0,
+						"__text": "mouseout"
+					},
+					{
+						"_value": 0,
+						"__text": "mouseover"
+					}
+				],
+				"_id": "2"
+			},
+			{
+				"text": "Observe o documento CSS....Ao abrir o documento em um navegador, a palavra Texto será exibida na cor",
+				"answer": [
+					{
+						"_value": 1,
+						"__text": "verde"
+					},
+					{
+						"_value": 0,
+						"__text": "roxa"
+					},
+					{
+						"_value": 0,
+						"__text": "azul"
+					},
+					{
+						"_value": 0,
+						"__text": "branca"
+					},
+					{
+						"_value": 0,
+						"__text": "vermelha"
+					}
+				],
+				"_id": "3"
+			}
+		]		
+	}
+}
